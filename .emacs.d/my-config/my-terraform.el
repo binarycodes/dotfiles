58f6b52c09c4814b95e0857b56a56ed779e46bcd
Sujoy Das <me@binarycodes.io>
2025-05-03 20:38:42 +0300

    format terraform on save

--- .emacs.d/my-config/my-terraform.el
+++ .emacs.d/my-config/my-terraform.el
@@ -6,7 +6,10 @@
 
   ;; if using package.el
   ;; :ensure t
-  :custom (terraform-indent-level 2)
+  :custom
+  (terraform-indent-level 2)
+  (terraform-format-on-save t)
+
   :config
   (defun my-terraform-mode-init ()
     ;; if you want to use outline-minor-mode
