69b401b8b051778dff1272eae4901e8c9fead7ae
Sujoy Das <me@binarycodes.io>
2025-09-21 14:17:04 +0300

    pick a random wallpaper when run in non-interactive mode

--- scripts/browse_wallpapers.sh
+++ scripts/browse_wallpapers.sh
@@ -6,12 +6,27 @@ files=(~/pictures/wallpaper/*)
 # exit silently if no files
 [ ${#files[@]} -eq 0 ] && exit 0
 
-echo -ne "Press any key to continue, q to quit."
 
-for f in "${files[@]}"; do
-    swww img "$f"
-    read -s -n 1 ans
-    [ "$ans" = q ] && echo && break
-done
+# detect if running in an interactive terminal
+if [ -t 0 ]; then
+    INTERACTIVE=1
+else
+    INTERACTIVE=0
+fi
 
-echo
+
+if [ "$INTERACTIVE" -eq 1 ]; then
+    echo -ne "Press any key to continue, q to quit."
+
+    for f in "${files[@]}"; do
+        swww img "$f"
+        read -s -n 1 ans
+        [ "$ans" = q ] && echo && break
+    done
+
+    echo
+else
+    # pick a random wallpaper and exit
+    RANDOM_INDEX=$(( RANDOM % ${#files[@]} ))
+    swww img "${files[$RANDOM_INDEX]}"
+fi
