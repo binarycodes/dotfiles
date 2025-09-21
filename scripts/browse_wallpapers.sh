c19eeb5ef7af20c96ddb038d22a6cee640bf529c
Sujoy Das <me@binarycodes.io>
2025-09-21 14:08:26 +0300

    improve the bash script - minimize prompts

--- scripts/browse_wallpapers.sh
+++ scripts/browse_wallpapers.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 
-for f in ~/pictures/wallpaper/*;
-do swww img "$f";
-   read -p "Press Enter to continue, q to quit: " ans;
-   [ "$ans" = q ] && break;
+shopt -s nullglob
+files=(~/pictures/wallpaper/*)
+
+# exit silently if no files
+[ ${#files[@]} -eq 0 ] && exit 0
+
+echo -ne "Press any key to continue, q to quit."
+
+for f in "${files[@]}"; do
+    swww img "$f"
+    read -s -n 1 ans
+    [ "$ans" = q ] && echo && break
 done
+
+echo
