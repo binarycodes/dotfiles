023513ed7d14cba0cce5cce4db396f59a7484b17
Sujoy Das <me@binarycodes.io>
2026-01-04 16:58:40 +0200

    sops config

--- .emacs.d/my-config/my-ansible.el
+++ .emacs.d/my-config/my-ansible.el
@@ -11,3 +11,11 @@
 (use-package ansible-vault
   :init (add-hook 'yaml-mode-hook 'ansible-vault-mode-maybe)
   :config (setq ansible-vault-password-file "~/.ansible/vault-password"))
+
+(use-package sops
+  :ensure t
+  :bind (("C-c C-c" . sops-save-file)
+         ("C-c C-k" . sops-cancel)
+         ("C-c C-d" . sops-edit-file))
+  :init
+  (global-sops-mode 1))
